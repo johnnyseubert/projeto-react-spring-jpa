aad28475a511414732124a88efe1936cfa0ce614
Jbseubert blasius <jbseubert@gmail.com>
2022-03-07 20:40:07 -0300

    Adicionando Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import App from './App';
 
@@ -8,4 +9,3 @@ ReactDOM.render(
     <App />
   </React.StrictMode>, document.getElementById('root')
 );
-
