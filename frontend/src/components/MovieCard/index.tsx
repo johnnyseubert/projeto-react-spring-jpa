3e5dd19006500d4fb8936ad101f47d9cedec8e6f
DATA53454 <data53454@datainfo.inf.br>
2022-03-08 13:17:37 -0300

    Navigation Buttons

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,9 +1,10 @@
 import MovieScore from "components/MovieScore";
+import { Link } from "react-router-dom";
 
 export default function MovieCard() {
 
    const movie = {
-      id: 1,
+      id: 10,
       image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
       title: "The Witcher",
       count: 2,
@@ -16,7 +17,11 @@ export default function MovieCard() {
          <div className="dsmovie-card-bottom-container mb-5">
             <h3>{movie.title}</h3>
             <MovieScore />
-            <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+
+            <Link to={`/form/${movie.id}`}>
+               <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+            </Link>
+
          </div>
       </div>
    )
