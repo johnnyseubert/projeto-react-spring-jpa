cb0345f60f9692f32ec87353e2a147a74b0afeeb
Jbseubert blasius <jbseubert@gmail.com>
2022-03-13 19:16:01 -0300

    fixing

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -29,9 +29,9 @@ function getFills(score: number) {
 }
 
 function Star({ fill }: StarProps) {
-   if (fill == 0) {
+   if (fill === 0) {
       return <StarEmpty />
-   } else if (fill == 0.5) {
+   } else if (fill === 0.5) {
       return <StarHalf />
    } else {
       return <StarFull />
